604a40e391b40aec6c69fbc9b1786a04e5fbafc8
Farah Erban <f-erban@Farah-Erbans-MacBook-Pro.local>
2021-04-08 16:10:28 +0100

    js file editing

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
 // Password character arrays
 
-var UppercaseChar =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const UppercaseChar =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
-var LowercaseChar =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const LowercaseChar =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-var NumbersChar =["0","1","2","3","4","5","6","7","8","9"];
+const NumbersChar =["0","1","2","3","4","5","6","7","8","9"];
 
-var SpecialChar =["!","?","@","£","$","%","&","+","#"];
+const SpecialChar =["!","?","@","£","$","%","&","+","#"];
 
 // Variable for user input
 // Prompt user to use at least 8 characters
 // Alert user if character length doesn't meet the requirement
-function generateOptions () {
-  var length = parseInt(prompt("How many characters would you like to use?"))
+function generatePassword () {
+  const length = parseInt(prompt("How many characters would you like to use?"))
 
   if (length < 8) {
     alert("Your password must contain at least 8 characters!")
@@ -27,23 +27,23 @@ function generateOptions () {
   }
 
 //Prompt for Uppercase character
-var shouldIncludeUppercase = confirm("Do you want to use Uppercase letters?");
+const shouldIncludeUppercase = confirm("Do you want to use Uppercase letters?");
 
 //Prompt for Lowercase character
-var shouldIncludeLowercase = confirm("Do you want to use Lowercase letters?");
+const shouldIncludeLowercase = confirm("Do you want to use Lowercase letters?");
 
 //Prompt for Special character
-var shouldIncludeSpecial = confirm("Do you want to use Special characters?");
+const shouldIncludeSpecial = confirm("Do you want to use Special characters?");
 
 //Prompt for Numbers character
-var shouldIncludeNumbers = confirm("Do you want to use Numerical characters?");
+const shouldIncludeNumbers = confirm("Do you want to use Numerical characters?");
 
 if (!shouldIncludeUppercase && !shouldIncludeLower && !shouldIncludeSpecial && !shouldIncludeNumbers) {
   alert("Your password must contain at least one special, numeric, lowercase, or uppercase character!");
   return;
 }
 
-var questionOptions = {
+const questionOptions = {
   length: length,
   special: shouldIncludeSpecial,
   numbers: shouldIncludeNumbers,
@@ -59,8 +59,8 @@ return questionOptions;
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
